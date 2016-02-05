3335484993b2d84565e6f6c47a06e3041761a0d7
zbobos <zbobos@localhost>
2016-02-05 16:43:58 -0500

    Starting phase

---
+++ javademo/src/edusaintjoe/cs/zbobos/javademo/JAVADEMO.java
@@ -0,0 +1,15 @@
+package edusaintjoe.cs.zbobos.javademo;
+
+public class JAVADEMO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i = 0;
+			while(i < 5) {
+				System.out.println("Hola pequano chicos, quiero Candy");
+				i = i + 1;
+			}
+				
+	}
+
+}
